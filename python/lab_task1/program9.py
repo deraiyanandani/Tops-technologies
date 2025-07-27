0cb0bc1e20fe07c3e5ce6ab89409006224e4841c
deraiyanandani <nrderaiya06@gmail.com>
2025-07-27 21:01:11 +0530

    Update program9.py

--- python/lab_task1/program9.py
+++ python/lab_task1/program9.py
@@ -1,11 +1,11 @@
 # count odd-even numbers
-lst=[22,11,42,13,58,59,54,21,23,26]
+lst=[22,11,42,13,33,59,54,21,23,26]
 even=0
 odd=0
 for i in lst:
     if i%2==0:
         even+=1
-        print("even=",even)
-    else:
+     else:
         odd+=1
-        print("odd=",odd)
+print("even=",even)
+print("odd=",odd)
