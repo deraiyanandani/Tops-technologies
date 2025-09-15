2828af370345ad83ce60461d05024f344ae99513
deraiyanandani <nrderaiya06@gmail.com>
2025-09-15 23:51:43 +0530

    Delete python/module2/assignment1 directory

--- python/module2/assignment1/program2.py
+++
@@ -1,7 +0,0 @@
-# Write a Python program to count occurrences of a substring in a string. 
-
-main_str=input("Enter the string::")
-sub_str=input("Enter the substring::")
-
-count=main_str.count(sub_str)
-print(count)
